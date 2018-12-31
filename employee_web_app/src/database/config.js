a1f23789e457b47798e8f57685ceed77c4644402
Christian Reyes <35509430+fcr3@users.noreply.github.com>
2018-12-31 09:39:24 -0800

    Updated config.js

--- employee_web_app/src/database/config.js
+++ employee_web_app/src/database/config.js
@@ -1,12 +1,7 @@
 import * as firebase from 'firebase';
 
 var config = {
-  apiKey: "AIzaSyD0BzL1Tblv5wE_lg4lEIV6v9mbK7Dt1OU",
-  authDomain: "experimental-database-fcr3.firebaseapp.com",
-  databaseURL: "https://experimental-database-fcr3.firebaseio.com",
-  projectId: "experimental-database-fcr3",
-  storageBucket: "experimental-database-fcr3.appspot.com",
-  messagingSenderId: "264350328196"
+
 };
 firebase.initializeApp(config);
 
